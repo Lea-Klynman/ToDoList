e986179b1179fbfb1dfa5955dc0caec674fb0032
ללי קלינמן <smalot2247@gmail.com>
2025-02-06 17:23:39 +0200

    change2

--- ToDoListServer/Program.cs
+++ ToDoListServer/Program.cs
@@ -2,70 +2,64 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Injecting the DbContext
-builder.Services.AddDbContext<ToDoDbContext>(/*options =>
-    options.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"))
-*/);
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAllOrigins",
-        builder => builder.AllowAnyOrigin()
-                          .AllowAnyMethod()
-                          .AllowAnyHeader());
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 0))
+    ));
 var app = builder.Build();
 
-// Use CORS before other middleware
-app.UseCors("AllowAllOrigins");
-
 app.UseSwagger();
-app.UseSwaggerUI(/*c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API V1");
-    c.RoutePrefix = string.Empty; // This allows you to access Swagger at the root address
-}*/);
+app.UseSwaggerUI();
+app.UseCors();
 
-// Get all items
+
+app.MapGet("/", () => "TodoList API is running");
+
 app.MapGet("/items", async (ToDoDbContext db) =>
 {
-    return await db.Items.ToListAsync();
+    var items = await db.Items.ToListAsync();
+    return Results.Ok(items);
 });
 
-// Add a new item
-app.MapPost("/items", async (ToDoDbContext db, Item newItem) =>
+app.MapGet("/items/{id}", async (int id, ToDoDbContext db) =>
+    await db.Items.FindAsync(id) is Item item ? Results.Ok(item) : Results.NotFound());
+
+
+app.MapPost("/", async (Item item, ToDoDbContext db) =>
 {
-    try
-    {
-        db.Items.Add(newItem);
-        await db.SaveChangesAsync();
-        return Results.Created($"/items/{newItem.Id}", newItem);
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem(ex.Message);
-    }
+    db.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/items/{item.Id}", item);
+
 });
 
-// Update an item
-app.MapPut("/items/{id}", async (ToDoDbContext db, int id, Item updatedItem) =>
+app.MapPut("/items/{id}", async (int id, bool iscomplete, ToDoDbContext db) =>
 {
-    var existingItem = await db.Items.FindAsync(id);
-    if (existingItem is null) return Results.NotFound();
+    var item = await db.Items.FindAsync(id);
+    if (item is null) return Results.NotFound();
 
-    existingItem.IsComplete = updatedItem.IsComplete;
+    item.IsComplete = iscomplete;
 
     await db.SaveChangesAsync();
     return Results.NoContent();
 });
 
-// Delete an item
-app.MapDelete("/items/{id}", async (ToDoDbContext db, int id) =>
+
+app.MapDelete("/items/{id}", async (int id, ToDoDbContext db) =>
 {
     var item = await db.Items.FindAsync(id);
     if (item is null) return Results.NotFound();
@@ -74,5 +68,82 @@ app.MapDelete("/items/{id}", async (ToDoDbContext db, int id) =>
     await db.SaveChangesAsync();
     return Results.NoContent();
 });
-
 app.Run();
+// using Microsoft.EntityFrameworkCore;
+// using TodoApi;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Injecting the DbContext
+// builder.Services.AddDbContext<ToDoDbContext>(/*options =>
+//     options.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"))
+// */);
+
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAllOrigins",
+//         builder => builder.AllowAnyOrigin()
+//                           .AllowAnyMethod()
+//                           .AllowAnyHeader());
+// });
+
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Use CORS before other middleware
+// app.UseCors("AllowAllOrigins");
+
+// app.UseSwagger();
+// app.UseSwaggerUI(/*c =>
+// {
+//     c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API V1");
+//     c.RoutePrefix = string.Empty; // This allows you to access Swagger at the root address
+// }*/);
+
+// // Get all items
+// app.MapGet("/items", async (ToDoDbContext db) =>
+// {
+//     return await db.Items.ToListAsync();
+// });
+
+// // Add a new item
+// app.MapPost("/items", async (ToDoDbContext db, Item newItem) =>
+// {
+//     try
+//     {
+//         db.Items.Add(newItem);
+//         await db.SaveChangesAsync();
+//         return Results.Created($"/items/{newItem.Id}", newItem);
+//     }
+//     catch (Exception ex)
+//     {
+//         return Results.Problem(ex.Message);
+//     }
+// });
+
+// // Update an item
+// app.MapPut("/items/{id}", async (ToDoDbContext db, int id, Item updatedItem) =>
+// {
+//     var existingItem = await db.Items.FindAsync(id);
+//     if (existingItem is null) return Results.NotFound();
+
+//     existingItem.IsComplete = updatedItem.IsComplete;
+
+//     await db.SaveChangesAsync();
+//     return Results.NoContent();
+// });
+
+// // Delete an item
+// app.MapDelete("/items/{id}", async (ToDoDbContext db, int id) =>
+// {
+//     var item = await db.Items.FindAsync(id);
+//     if (item is null) return Results.NotFound();
+
+//     db.Items.Remove(item);
+//     await db.SaveChangesAsync();
+//     return Results.NoContent();
+// });
+
+// app.Run();
--- ToDoListServer/obj/Debug/net7.0/ToDoApi.AssemblyInfo.cs
+++ ToDoListServer/obj/Debug/net7.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb5cdd55a5ef2323531b1cbba93f17236c13624")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+222396db7da04a5e4b12bac81c6c52f12752c821")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
